77da225dedd6e9ef9fdf33864dd326a6b12767f0
Gocha Malania <97460075+gochamalania@users.noreply.github.com>
2026-05-09 00:54:31 +0400

    initial commit

---
+++ lecture2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace lecture2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+
+
+            //string strPoint = "100";
+            //int pont = Convert.ToInt32(strPoint); //100
+
+
+
+            //int.TryParse("100", out int x);
+            //Console.WriteLine(x);
+
+            //bool parsedOk = int.TryParse("100", out int x);
+
+            //bool isTrue = true;
+            //bool isNotTrue = false;
+
+
+            // Console.WriteLine(typeof(int));
+
+           // Console.WriteLine();
+
+
+           // int num1 = 5;
+           // num1++;
+           // num1--;
+           //
+           // Console.WriteLine(--num1);
+           
+           
+
+
+
+
+        }
+    }
+}
