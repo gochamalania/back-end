fd1aeb06db793dc028ed0498e7782b58df601068
Gocha Malania <97460075+gochamalania@users.noreply.github.com>
2026-05-12 00:34:11 +0400

    homerowk1

---
+++ homeWork/homeWork1/homeWork1/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace homeWork1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+        
+        // დავალება 1
+        // დაწერეთ C# Sharp პროგრამა, რომლითაც მომხმარებელი შეიყვანს ამომრჩევლის ასაკს და პროგრამა
+        //     განსაზღვრავს, აქვს თუ არა მას არჩევნებზე ხმის მიცემის უფლება.
+        //     შესაყვანი სატესტო მონაცემი: 18
+        // მოსალოდნელი შედეგი: გილოცავ! ხმის მიცემის უფლება გაქვთ.
+        //     –––––––––––––––––––––––
+        // შესაყვანი სატესტო მონაცემი: 15
+        // მოსალოდნელი შედეგი: სამწუხაროდ ხმის მიცემის უფლება ჯერ არ გაქვთ.
+        // Console.WriteLine("enter your age: ");
+        // int age = Convert.ToByte(Console.ReadLine());
+        //
+        // if (age >= 18)
+        // {
+        //     Console.WriteLine("Congratulations! You have the right to vote.");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Unfortunately, you don't have the right to vote yet.");
+        // }
+        
+        
+        // დავალება 2
+        // დაწერეთ C# პროგრამა, რომელიც დაადგენს სამ რიცხვს შორის უდიდესს.
+        //     შესაყვანი სატესტო მონაცემი:
+        // შეიყვანეთ პირველი რიცხვი: 25
+        // შეიყვანეთ მე-2 რიცხვი: 63
+        // შეიყვანეთ მე-3 რიცხვი: 10
+        // მოსალოდნელი შედეგი: მე-2 რიცხვი მაქსიმალურია წარმოდგენილ რიცხვებს შორის.
+        // Console.WriteLine("Enter the first number: ");
+        // int num1 = Convert.ToByte(Console.ReadLine());
+        //
+        // Console.WriteLine("Enter the second number: ");
+        // int num2 = Convert.ToByte(Console.ReadLine());
+        //
+        // Console.WriteLine("Enter the third number: ");
+        // int num3 = Convert.ToByte(Console.ReadLine());
+        //
+        // if (num1 > num2 && num1 > num3)
+        // {
+        //     Console.WriteLine("The first number is the maximum among the numbers presented.");
+        // }
+        // else if (num2 > num3 && num2 > num1)
+        // {
+        //     Console.WriteLine("The second number is the maximum among the numbers presented.");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("The third number is the maximum among the numbers presented.");
+        // }
+        
+        
+        
+        
+        // დავალება 3
+        // დაწერეთ C# პროგრამა ორი მოცემული მთელი რიცხვის ჯამის გამოსათვლელად. თუ ეს ორი რიცხვი
+        //     ერთნაირია, მაშინ დააბრუნეთ გასამმაგებული მათი ჯამი.
+        //     შეყვანის ნიმუში:
+        // 1, 2
+        // 3, 2
+        // 2, 2
+        // მოსალოდნელი შედეგი:
+        // 3
+        // 5
+        // 12
+        // Console.WriteLine("Enter first number: ");
+        // int num1 = Convert.ToInt32(Console.ReadLine());
+        //
+        // Console.WriteLine("Enter second number: ");
+        // int num2 = Convert.ToInt32(Console.ReadLine());
+        //
+        // int result = (num1 == num2) ? (num1 + num2) *3 : (num1 + num2);
+        //
+        // Console.WriteLine("result " +  result);
+        
+    }
+}
