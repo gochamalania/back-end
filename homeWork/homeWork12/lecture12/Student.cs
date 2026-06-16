33d79273dffe4e51959660e54fbfe3764a529413
Gocha Malania <97460075+gochamalania@users.noreply.github.com>
2026-06-16 20:29:16 +0400

    homeWork12

---
+++ homeWork/homeWork12/lecture12/Student.cs
@@ -0,0 +1,35 @@
+﻿namespace lecture12;
+
+public class Student
+{
+    // Giorgi,Beridze,25,g.beridze@email.com,599123456,90
+    public string FirstName {get; set;}
+    public string LastName {get; set;}
+    public int Age {get; set;}
+    public string Email {get; set;}
+    public string Phone {get; set;}
+    public int Score {get; set;}
+
+
+    public Student(string firstName, string lastName, int age, string email, string phone, int score)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+        Email = email;
+        Phone = phone;
+        Score = score;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine(FirstName + " " + LastName + ", Age: "  + Age + " " + Email + " " + Phone + ", Score: " + Score);
+    }
+    
+    
+    
+    // ვიპოვოთ სტუდენტები, რომელსაც დაბალი ქულა აქვს;
+    // ასაკით დიდი;
+    // ყველა სტუდენტის საშუალო ქულა;
+    // დაასორტირეთ სტუდენტების მასივი
+}
